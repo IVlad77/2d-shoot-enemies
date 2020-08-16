dc0c6351fb5f680a4219aad8e1649ef10394cb4b
IVlad77 <69730273+IVlad77@users.noreply.github.com>
2020-08-16 14:59:02 +0300

    All

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,67 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEditor.Experimental;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    public float speed;
+    private UnityEngine.Vector2 moveAmount;
+    private Animator anim;
+
+    public float health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+        
+        UnityEngine.Vector2 moveInput = new UnityEngine.Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveAmount = moveInput.normalized * speed;
+
+
+        if (moveInput != UnityEngine.Vector2.zero)
+        {
+            anim.SetBool("isRunning", true);
+
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+    }
+    public void TakeDamage(int damageAmount)
+    {
+        health -= damageAmount;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveAmount * Time.fixedDeltaTime);
+    }
+
+    
+
+
+}
+
+
+
